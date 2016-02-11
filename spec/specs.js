3dadfc732d7bab6fafd41ac5a9415bad3234613e
Nick Embrey <cne.embrey@gmail.com>
2016-02-11 10:26:50 -0800

    Add boilerplate code to specs.js.

---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('pingPong', function() {
+  it("counts to a number, outputting 'ping' for every number divisible by 3, 'pong' for every number divisible by 5, and 'pingpong' for every number divisible by both", function() {
+    expect(pingPong(5).to.equal(enterValue);
+  });
+});
