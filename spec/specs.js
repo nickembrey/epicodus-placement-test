4b7ecaee16bcea917e48b31e343fb1475e10881d
Nick Embrey <cne.embrey@gmail.com>
2016-02-11 10:43:44 -0800

    Change description of first spec to be accurate.

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
 describe('pingPong', function() {
-  it("returns 'ping' for a number divisible by 3, 'pong' for a number divisible by 5, and 'pingpong' for a number divisible by both, otherwise returns the input number", function() {
+  it("returns the input number when the number is not divisible by 3 or 5", function() {
     expect(pingPong(16)).to.equal(16);
   });
 });
