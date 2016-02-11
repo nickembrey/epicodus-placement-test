9917715b24081e8322ca27373f55b232974ef9ab
Nick Embrey <cne.embrey@gmail.com>
2016-02-11 10:55:20 -0800

    Make pingPong return 'pong' for number divisible by 5.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 var pingPong = function(number) {
-  if(number % 3 === 0) {
+  if(number % 5 === 0) {
+    return "pong";
+  }
+  else if(number % 3 === 0) {
     return "ping";
   } else {
     return number;
