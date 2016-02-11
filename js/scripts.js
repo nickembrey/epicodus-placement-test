75d53a41a367b8dce47e54cbe81296e1e8ed03fc
Nick Embrey <cne.embrey@gmail.com>
2016-02-11 13:07:32 -0800

    Add main loop to playPingPong function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 var pingPong = function(input) {
   if( (input % 5 === 0) && (input % 3 === 0) ) {
-    return "pingpong";
+    return 'pingpong';
   }
   else if(input % 5 === 0) {
-    return "pong";
+    return 'pong';
   }
   else if(input % 3 === 0) {
-    return "ping";
+    return 'ping';
   } else {
     return input;
   }
@@ -14,12 +14,16 @@ var pingPong = function(input) {
 
 var playPingPong = function(userInput) {
   if (userInput % 1 != 0) {
-    return "Oops! That’s not an integer. Please input an integer.";
+    return 'Oops! That’s not an integer. Please input an integer.';
+  } else {
+    for(inputCounter = 0; inputCounter < userInput; inputCounter++) {
+      $('#ping-pong-list').append('<li>' + pingPong(inputCounter) + '</li>');
+    }
   }
 };
 
 $(function() {
   $('#start-ping-pong').click(function() {
-    alert("jQuery working with button!");
+    $('#main').append('<ul id="ping-pong-list"></ul>')
   });
 });
