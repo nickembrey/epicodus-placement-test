3c9705811ac3d9c98f1387ca8db7de2026aa6a97
Nick Embrey <cne.embrey@gmail.com>
2016-02-11 10:46:24 -0800

    Add divisible by 3 but not 5 spec.

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('pingPong', function() {
   it("returns the input number when the number is not divisible by 3 or 5", function() {
     expect(pingPong(16)).to.equal(16);
   });
+
+  it("returns ping when the number is divisible by 3 but not 5", function() {
+    expect(pingPong(9)).to.equal("ping");
+  });
 });
